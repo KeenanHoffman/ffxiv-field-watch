537077030624f0a0cd526e4399cac6bc2fffbb04
KeenanHoffman <keenan.m.hoffman@gmail.com>
2017-01-31 20:51:33 -0700

    Fix alarm sound

--- src/components/Alarms/Alarms.js
+++ src/components/Alarms/Alarms.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Alarms.css';
 import Alarm from './Alarm'
+import sound from './alarm.mp3'
 
 var Alarms = React.createClass({
 	render: function() {
 		var epoch = this.props.epoch
 		var time = this.props.time
 		var alarms = this.props.alarms.map(function(alarm, key) {
-			var tone = 'http://download1875.mediafire.com/0o0xdoysv2tg/koqi9jp1yslu71k/Chocobo+Wark%21+Ringtone%28Dracotonis%29.mp3'
 			return <Alarm time={time} alarm={alarm} epoch={epoch} key={key} playAlarm={function() {
-					new Audio(tone).play()
+					new Audio(sound).play()
 				}} />
 		})
 		return (
